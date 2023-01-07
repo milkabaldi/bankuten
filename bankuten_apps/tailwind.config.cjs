a45331228449d03e9975866853b6cc10ef63734f
milkabaldi <justmainc8@gmail.com>
2023-01-07 17:07:36 +0200

    Start

---
+++ bankuten_apps/tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#0B4141",
+        dimWhite: "#fff",
+        dimBlue: "#0B4141",
+        dimPeach: '#FFCCBC',
+        dimLightGrey: 'rgba(245, 245, 245, 0.6);',
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};

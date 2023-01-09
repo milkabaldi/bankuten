3cbddcc60d8cad4f5a64555f269053945bb2707f
milkabaldi <justmainc8@gmail.com>
2023-01-09 23:25:56 +0200

    Add images and data

---
+++ bankuten_apps/src/constants/images.js
@@ -0,0 +1,60 @@
+import Heart from '../assets/heart.png'
+import Plane from '../assets/plane.png'
+import Hospital from '../assets/hospital.png'
+import House from '../assets/house.png'
+import First from '../assets/first.png'
+import Second from '../assets/second.png'
+import Reviewer1 from '../assets/reviewer1.png'
+import Reviewer2 from '../assets/reviewer2.png'
+import Reviewer3 from '../assets/reviewer3.png'
+import Company1 from '../assets/company1.png'
+import Company2 from '../assets/company2.png'
+import Company3 from '../assets/company3.png'
+import Company4 from '../assets/company4.png'
+import Company5 from '../assets/company5.png'
+import Company6 from '../assets/company6.png'
+import Company7 from '../assets/company7.png'
+import Company8 from '../assets/company8.png'
+import Company9 from '../assets/company9.png'
+import Company10 from '../assets/company10.png'
+import Company11 from '../assets/company11.png'
+import Icon1 from '../assets/icon1-1.png'
+import Icon2 from '../assets/icon1-2.png'
+import Icon3 from '../assets/icon1-3.png'
+import Icon4 from '../assets/icon1-4.png'
+import Icon5 from '../assets/icon2-1.png'
+import Icon6 from '../assets/icon2-2.png'
+import Icon7 from '../assets/icon2-3.png'
+import Icon8 from '../assets/icon2-4.png'
+
+export default{
+    Heart,
+    Plane,
+    Hospital,
+    House,
+    First,
+    Second,
+    Reviewer1,
+    Reviewer2,
+    Reviewer3,
+    Company1,
+    Company2,
+    Company3,
+    Company4,
+    Company5,
+    Company6,
+    Company7,
+    Company8,
+    Company9,
+    Company10,
+    Company11,
+    Icon1,
+    Icon2,
+    Icon3,
+    Icon4,
+    Icon5,
+    Icon6,
+    Icon7,
+    Icon8,
+
+}
